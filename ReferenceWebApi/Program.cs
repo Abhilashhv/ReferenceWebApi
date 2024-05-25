faf82c1dba9f5d842c6fcc0cebd4bdc912c9966d
Abhilash Kallampunathil <abhilash.kallampunathil@hitachids.com>
2024-05-25 14:52:16 +0530

    updated

--- ReferenceWebApi/Program.cs
+++ ReferenceWebApi/Program.cs
@@ -7,7 +7,7 @@ using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 //var keyVaultEndpoint = new Uri("https://courses-keyvault.vault.azure.net/");
-var keyVaultEndpoint = new Uri("10.0.1.4");
+var keyVaultEndpoint = new Uri("https://courses-keyvault.privatelink.vaultcore.azure.net");
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
