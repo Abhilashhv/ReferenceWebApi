42e9e54f7bf099c043e4acf3da90b3960b35598a
Abhilash Kallampunathil <abhilash.kallampunathil@hitachids.com>
2024-05-25 10:33:39 +0530

    added keyvault for secrets

--- ReferenceWebApi/Program.cs
+++ ReferenceWebApi/Program.cs
@@ -2,8 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ReferenceWebApi.Data;
 using ReferenceWebApi.Services;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri("https://courses-keyvault.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
@@ -11,7 +15,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
+//var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
+var connectionString = builder.Configuration["AzureSqlConnection"];
+
 builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(connectionString));
 builder.Services.AddTransient<ICourseRepo, CourseRepo>();
 var app = builder.Build();
