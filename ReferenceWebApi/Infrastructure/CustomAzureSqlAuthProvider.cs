1b2ecd92d187578ba9fdd2adac3da95ed9c0c6c4
Abhilash Kallampunathil <abhilash.kallampunathil@hitachids.com>
2024-05-26 11:15:49 +0530

    added sql server connection using managed identity

---
+++ ReferenceWebApi/Infrastructure/CustomAzureSqlAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+
+namespace ReferenceWebApi.Infrastructure
+{
+    public class CustomAzureSqlAuthProvider : SqlAuthenticationProvider
+    {
+        private static readonly string[] _azureSqlScopes = new[]
+        {
+            "https://database.windows.net//.default"
+        };
+        private static readonly TokenCredential _credential = new DefaultAzureCredential();
+        public override async Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
+        {
+            var tokenRequestContext = new TokenRequestContext(_azureSqlScopes);
+            var tokenResult = await _credential.GetTokenAsync(tokenRequestContext, default);
+            return new SqlAuthenticationToken(tokenResult.Token, tokenResult.ExpiresOn);
+        }
+        public override bool IsSupported(SqlAuthenticationMethod authenticationMethod) => authenticationMethod.Equals(SqlAuthenticationMethod.ActiveDirectoryDeviceCodeFlow);
+    }
+}
--- ReferenceWebApi/Program.cs
+++ ReferenceWebApi/Program.cs
@@ -3,12 +3,15 @@ using Microsoft.Extensions.Options;
 using ReferenceWebApi.Data;
 using ReferenceWebApi.Services;
 using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using ReferenceWebApi.Infrastructure;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri("https://courses-keyvault.vault.azure.net/");
+//var keyVaultEndpoint = new Uri("https://courses-keyvault.vault.azure.net/");
 //var keyVaultEndpoint = new Uri("http://courses-keyvault.privatelink.vaultcore.azure.net");
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
@@ -16,10 +19,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
-var connectionString = builder.Configuration["AzureSqlConnection"];
+var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
+//var connectionString = builder.Configuration["AzureSqlConnection"];
 
-builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<DataContext>(Options =>
+{
+    SqlAuthenticationProvider.SetProvider(
+                    SqlAuthenticationMethod.ActiveDirectoryDeviceCodeFlow,
+                    new CustomAzureSqlAuthProvider());
+    var sqlConnection = new SqlConnection(connectionString);
+    Options.UseSqlServer(sqlConnection); 
+});
 builder.Services.AddTransient<ICourseRepo, CourseRepo>();
 var app = builder.Build();
 
